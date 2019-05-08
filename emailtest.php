0c6fd0d0996698a10740d84a81e97f220d130e25
Luis Valencia <lvalencia@csumb.edu>
2019-05-08 17:29:18 +0000

    testing

--- emailtest.php
+++ emailtest.php
@@ -1,10 +1,25 @@
 <?php
-// the message
-$msg = "First line of text\nSecond line of text";
+$subject = 'subject';
+$message = 'message';
+$to = 'luisv555@gmail.com';
+$mail ='lvalencia@csumb.edu';
+$type = 'plain'; // or HTML
+$charset = 'utf-8';
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+$mail     = 'no-reply@'.str_replace('www.', '', $_SERVER['SERVER_NAME']);
+$uniqid   = md5(uniqid(time()));
+$headers  = 'From: '.$mail."\n";
+$headers .= 'Reply-to: '.$mail."\n";
+$headers .= 'Return-Path: '.$mail."\n";
+//$headers .= 'Message-ID: <'.$uniqid.'@'.$_SERVER['SERVER_NAME'].">\n";
+$headers .= 'MIME-Version: 1.0'."\n";
+$headers .= 'Date: '.gmdate('D, d M Y H:i:s', time())."\n";
+$headers .= 'X-Priority: 3'."\n";
+$headers .= 'X-MSMail-Priority: Normal'."\n";
+$headers .= 'Content-Type: multipart/mixed;boundary="----------'.$uniqid.'"'."\n";
+//$headers .= '------------'.$uniqid."\n";
+$headers .= 'Content-type: text/'.$type.';charset='.$charset.''."\n";
+$headers .= 'Content-transfer-encoding: 7bit';
 
-// send email
-mail("luisv555@gmail.com","My subject",$msg);
+mail($to, $subject, $message, $headers);
 ?>
